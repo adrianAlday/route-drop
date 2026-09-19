b60d806ef83ccebcb01fc4d79c83bae6e60f73c9
adrianAlday <adrian.alday@gmail.com>
2026-09-19 18:53:15 -0400

    bouncer

--- app/_components/Map.tsx
+++ app/_components/Map.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Bouncer from "./Bouncer";
+
 const HomeMap = () => {
-  return <div className="w-dvw">asdf</div>;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className="w-dvw">
+      <Bouncer classNames={loading ? "block" : "hidden"} />
+
+      <div className={`${loading ? "hidden" : "block"} relative`}>asdf</div>
+    </div>
+  );
 };
 
 export default HomeMap;
