2c7d0ea5d05058916a2711a89aee12720dd61b6e
adrianAlday <adrian.alday@gmail.com>
2026-09-19 19:29:36 -0400

    get route data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
 import HomeMap from "./_components/HomeMap";
 
-const HomePage = () => {
+const HomePage = async ({ searchParams }) => {
+  const resolvedParams = { ...(await searchParams) };
+
+  const getRoute = async (id: string) =>
+    await fetch(`https://api.footpathapp.com/v2/routes/${id}`, {
+      next: { revalidate: 60 * 60 },
+    })
+      .then(async (response) => await response.json())
+      .catch((error) => {
+        console.error(`Route Error: ${error}`);
+      });
+
+  const routeData = await Promise.all(
+    resolvedParams.r.split(",").map((routeId: string) => getRoute(routeId)),
+  );
+
   return (
     <main>
       <HomeMap />
