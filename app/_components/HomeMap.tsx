0abddbf58c1b1a04def17fb32699f5d04ea8072f
adrianAlday <adrian.alday@gmail.com>
2026-09-19 21:24:23 -0400

    show map

--- app/_components/HomeMap.tsx
+++ app/_components/HomeMap.tsx
@@ -3,6 +3,10 @@
 import { useState, useEffect } from "react";
 import Bouncer from "./Bouncer";
 import { LatLngTuple } from "@googlemaps/polyline-codec";
+import { getById, zoom, minZoom, setupMap } from "../_utils/map";
+import { centerMean, featureCollection, point } from "@turf/turf";
+import * as maplibreGl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
 
 export type Route = {
   id: string;
@@ -20,13 +24,78 @@ type HomeMapProps = {
 const HomeMap = ({ routeData }: HomeMapProps) => {
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {}, []);
+  const mapContainerId = "map";
+
+  useEffect(() => {
+    if (!getById(mapContainerId)) {
+      return;
+    }
+
+    const center = centerMean(
+      featureCollection(routeData.map((route) => point(route.coordinates[0]))),
+    ).geometry.coordinates.reverse() as [number, number];
+
+    const initialCoordinates = center;
+
+    const initialPosition = {
+      center: initialCoordinates,
+      zoom,
+    };
+
+    const mapInstance = new maplibreGl.Map({
+      container: mapContainerId,
+      ...initialPosition,
+      minZoom,
+      attributionControl: false,
+      localIdeographFontFamily: "-apple-system, BlinkMacSystemFont, sans-serif",
+    });
+
+    console.log("center", center);
+
+    setupMap(mapInstance);
+
+    const geolocateControl = new maplibreGl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      showUserLocation: true,
+      showAccuracyCircle: true,
+    });
+
+    mapInstance.addControl(geolocateControl, "top-right");
+
+    mapInstance.addControl(
+      new maplibreGl.NavigationControl({
+        visualizePitch: false,
+        visualizeRoll: false,
+        showZoom: true,
+        showCompass: false,
+      }),
+      "top-right",
+    );
+
+    console.log("hiya");
+
+    mapInstance.on("load", () => {
+      mapInstance.setProjection({
+        type: "globe",
+      });
+
+      setLoading(false);
+
+      console.log("load");
+    });
+
+    console.log("end");
+  }, [routeData]);
 
   return (
     <div className="w-dvw">
       <Bouncer classNames={loading ? "block" : "hidden"} />
 
-      <div className={`${loading ? "hidden" : "block"} relative`}>asdf</div>
+      <div className={`${loading ? "hidden" : "block"} relative`}>
+        <div id={mapContainerId} className={"h-dvh"} />
+      </div>
     </div>
   );
 };
---
+++ app/_utils/map.ts
@@ -0,0 +1,110 @@
+import * as maplibreGl from "maplibre-gl";
+import { Map, SourceSpecification } from "maplibre-gl";
+import maplibreContour from "maplibre-contour";
+
+export const zoom = 16;
+
+export const minZoom = 1;
+
+export const getById = (id: string) =>
+  document.getElementById(id) as HTMLElement;
+
+export const setupMap = (mapInstance: Map) => {
+  mapInstance.setStyle("https://tiles.openfreemap.org/styles/bright", {
+    transformStyle: (_previousStyle, nextStyle) => {
+      const mapterhornSource = {
+        type: "raster-dem",
+        url: "https://tiles.mapterhorn.com/tilejson.json",
+      } as SourceSpecification;
+
+      nextStyle.sources.hillshadeSource = mapterhornSource;
+      nextStyle.layers.push({
+        id: "hills",
+        type: "hillshade",
+        source: "hillshadeSource",
+        layout: { visibility: "visible" },
+        paint: { "hillshade-shadow-color": "rgb(71,59,36)" },
+      });
+
+      nextStyle.sources.terrainSource = mapterhornSource;
+      nextStyle.terrain = {
+        source: "terrainSource",
+      };
+
+      const demSource = new maplibreContour.DemSource({
+        url: "https://tiles.mapterhorn.com/{z}/{x}/{y}.webp",
+        maxzoom: 13,
+        worker: true,
+        cacheSize: 100,
+        timeoutMs: 10_000,
+      });
+      demSource.setupMaplibre(maplibreGl);
+
+      nextStyle.sources.contourSource = {
+        type: "vector",
+        tiles: [
+          demSource.contourProtocolUrl({
+            multiplier: 3.28084,
+            thresholds: {
+              1: [10, 100],
+            },
+            contourLayer: "contours",
+            elevationKey: "ele",
+            levelKey: "level",
+            extent: 4096,
+            buffer: 1,
+          }),
+        ],
+        maxzoom: 15,
+      };
+      nextStyle.layers.push({
+        id: "contourLines",
+        type: "line",
+        source: "contourSource",
+        "source-layer": "contours",
+        paint: {
+          "line-color": "rgba(71,59,36,0.33)",
+          "line-width": ["match", ["get", "level"], 1, 1, 0.5],
+        },
+      });
+      nextStyle.layers.push({
+        id: "contourLabels",
+        type: "symbol",
+        source: "contourSource",
+        "source-layer": "contours",
+        filter: [">", ["get", "level"], 0],
+        layout: {
+          "symbol-placement": "line",
+          "text-size": 12,
+          "text-field": ["concat", ["number-format", ["get", "ele"], {}], "'"],
+          "text-font": ["-apple-system", "BlinkMacSystemFont", "sans-serif"],
+        },
+        paint: {
+          "text-color": "rgba(71,59,36,0.66)",
+          "text-halo-color": "rgba(247,248,250,0.66)",
+          "text-halo-width": 0.66,
+        },
+      });
+
+      // fallback
+      // recent issue: https://github.com/hyperknot/openfreemap/issues/112
+
+      // nextStyle.sources.openmaptiles = {
+      //   type: "vector",
+      //   tiles: [
+      //     "https://tiles.openfreemap.org/planet/20260513_001001_pt/{z}/{x}/{y}.pbf",
+      //   ],
+      //   minzoom: 0,
+      //   maxzoom: 14,
+      // };
+
+      return nextStyle;
+    },
+  });
+  mapInstance.dragRotate.disable();
+  mapInstance.touchZoomRotate.disableRotation();
+  mapInstance.keyboard.disable();
+
+  mapInstance.setMaxPitch(0);
+  mapInstance.touchPitch.disable();
+};
