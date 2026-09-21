5f9e6f159ee14f0f6d7ddd54dd78638792ff9b79
adrianAlday <adrian.alday@gmail.com>
2026-09-21 12:22:34 -0400

    dark dot bouncer

--- app/_components/Bouncer.tsx
+++ app/_components/Bouncer.tsx
@@ -7,11 +7,11 @@ const Bouncer = ({ classNames }: BouncerProps) => (
     className={`mt-9 sm:mt-7 flex justify-center animate-pulse ${classNames}`}
   >
     <div className="flex space-x-2">
-      <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce [animation-delay:-0.3s]" />
+      <div className="rounded-full h-2 w-2 bg-[rgb(10,10,10)] animate-bounce [animation-delay:-0.3s]" />
 
-      <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce [animation-delay:-0.15s]" />
+      <div className="rounded-full h-2 w-2 bg-[rgb(10,10,10)] animate-bounce [animation-delay:-0.15s]" />
 
-      <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce" />
+      <div className="rounded-full h-2 w-2 bg-[rgb(10,10,10)] animate-bounce" />
     </div>
   </div>
 );
