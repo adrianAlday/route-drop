5f52070cf06501208515a15895ad46b021fd26a0
adrianAlday <adrian.alday@gmail.com>
2026-09-21 18:10:03 -0400

    bouncer color

--- app/_components/Bouncer.tsx
+++ app/_components/Bouncer.tsx
@@ -7,11 +7,11 @@ const Bouncer = ({ classNames }: BouncerProps) => (
     className={`mt-9 sm:mt-7 flex justify-center animate-pulse ${classNames}`}
   >
     <div className="flex space-x-2">
-      <div className="rounded-full h-2 w-2 bg-[rgb(10,10,10)] animate-bounce [animation-delay:-0.3s]" />
+      <div className="rounded-full h-2 w-2 border border-1 border-[rgb(1,8,43)] bg-[rgb(234,57,128))] animate-bounce [animation-delay:-0.3s]" />
 
-      <div className="rounded-full h-2 w-2 bg-[rgb(10,10,10)] animate-bounce [animation-delay:-0.15s]" />
+      <div className="rounded-full h-2 w-2 border border-1 border-[rgb(1,8,43)] bg-[rgb(217,252,82)] animate-bounce [animation-delay:-0.15s]" />
 
-      <div className="rounded-full h-2 w-2 bg-[rgb(10,10,10)] animate-bounce" />
+      <div className="rounded-full h-2 w-2 border border-1 border-[rgb(1,8,43)] bg-[rgb(57,128,234))] animate-bounce" />
     </div>
   </div>
 );
