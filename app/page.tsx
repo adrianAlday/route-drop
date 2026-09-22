a2615f5792d4c94640957d00b5a9982493c3b63a
adrianAlday <adrian.alday@gmail.com>
2026-09-22 11:37:36 -0400

    renaming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Params } from "./_utils/types";
 import { decode } from "@googlemaps/polyline-codec";
 import * as turf from "@turf/turf";
-import HomeMap, { Route } from "./_components/RouteMap";
+import RouteMap, { Route } from "./_components/RouteMap";
 import Builder from "./_components/Builder";
 
 export const getRoute = async (id: string) =>
@@ -69,7 +69,7 @@ const HomePage = async ({ searchParams }: HomePageProps) => {
 
   return (
     <main>
-      {routeData.length ? <HomeMap routeData={routeData} /> : <Builder />}
+      {routeData.length ? <RouteMap routeData={routeData} /> : <Builder />}
     </main>
   );
 };
